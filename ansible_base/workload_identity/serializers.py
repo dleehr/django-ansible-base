ac92494ee7b0800645405df57bdcb254ef13b929
Dan Leehr <dleehr@redhat.com>
2025-12-19 11:07:02 -0500

    Rename workload_details to claims and add audience params

--- ansible_base/workload_identity/serializers.py
+++ ansible_base/workload_identity/serializers.py
@@ -4,11 +4,11 @@ from rest_framework import serializers
 
 
 class WorkloadIdentitySerializer(serializers.Serializer):
-    workload_details = serializers.JSONField()  # This just guarantees the incoming data can be JSON-serialized
+    claims = serializers.JSONField()  # This just guarantees the incoming data can be JSON-serialized
     audience = serializers.CharField()
 
     def to_representation(self, instance):
         ret = super().to_representation(instance)
         # when we POST this we don't want to send nested JSON, we want to send a JSON string
-        ret['workload_details'] = json.dumps(ret['workload_details'], sort_keys=True)
+        ret['claims'] = json.dumps(ret['claims'], sort_keys=True)
         return ret
