cb2d60ac11637338dcc5c0ad34a84d29bf49c31a
Dan Leehr <dleehr@redhat.com>
2025-12-18 20:15:25 -0500

    This kinda works but breaks /o/

--- ansible_base/oauth2_provider/urls.py
+++ ansible_base/oauth2_provider/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path, re_path
 from oauth2_provider import views as oauth_views
-
+from oauth2_provider.urls import oidc_urlpatterns
 from ansible_base.lib.routers import AssociationResourceRouter
 from ansible_base.oauth2_provider import views as oauth2_provider_views
 from ansible_base.oauth2_provider.apps import Oauth2ProviderConfig
@@ -28,9 +28,13 @@ api_version_urls = [
     path('', include(router.urls)),
 ]
 
+oauth_urls = [
+    re_path(r'^$', oauth2_provider_views.ApiOAuthAuthorizationRootView.as_view(), name='oauth_authorization_root_view'),
+    re_path(r"^authorize/$", oauth_views.AuthorizationView.as_view(), name="authorize"),
+    re_path(r"^token/$", oauth2_provider_views.TokenView.as_view(), name="token"),
+    re_path(r"^revoke_token/$", oauth_views.RevokeTokenView.as_view(), name="revoke-token"),
+] + oidc_urlpatterns
+
 root_urls = [
-    re_path(r'^o/$', oauth2_provider_views.ApiOAuthAuthorizationRootView.as_view(), name='oauth_authorization_root_view'),
-    re_path(r"^o/authorize/$", oauth_views.AuthorizationView.as_view(), name="authorize"),
-    re_path(r"^o/token/$", oauth2_provider_views.TokenView.as_view(), name="token"),
-    re_path(r"^o/revoke_token/$", oauth_views.RevokeTokenView.as_view(), name="revoke-token"),
+    re_path(r"^o/", include((oauth_urls, 'oauth2_provider'))),
 ]
